ace5be96787bccbf213b80ff220308528d0ad9fa
mrLopez <lopesdenis@mail.ru>
2025-01-07 15:54:54 -0500

    add meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,31 +4,32 @@ export default defineNuxtConfig({
     "bootstrap/dist/css/bootstrap.min.css", // Подключение CSS Bootstrap
     "@/assets/css/main.css" // Подключение файла main.css
   ],
-
-  app: {
-    head: {
-      link: [
-        {
-          rel: "stylesheet",
-          href: "https://unpkg.com/ionicons@5.5.2/dist/ionicons.min.css", // Подключение CSS Ionicons
-        },
-      ],
-      script: [
-        {
-          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js", // Подключение JS Bootstrap через CDN
-          type: "text/javascript",
-        },
-        {
-          type: "module",
-          src: "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js", // Подключение ES-модуля Ionicons
-        },
-        {
-          nomodule: true,
-          src: "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js", // Подключение fallback-скрипта Ionicons
-        },
-      ],
-    },
+  head: {
+    title: 'Carbon Nurture',
+    meta: [
+      { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+      // другие мета-теги
+    ],
+    link: [
+      {
+        rel: "stylesheet",
+        href: "https://unpkg.com/ionicons@5.5.2/dist/ionicons.min.css", // Подключение CSS Ionicons
+      },
+    ],
+    script: [
+      {
+        src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js", // Подключение JS Bootstrap через CDN
+        type: "text/javascript",
+      },
+      {
+        type: "module",
+        src: "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js", // Подключение ES-модуля Ionicons
+      },
+      {
+        nomodule: true,
+        src: "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js", // Подключение fallback-скрипта Ionicons
+      },
+    ],
   },
-
   compatibilityDate: "2024-12-07",
 });
