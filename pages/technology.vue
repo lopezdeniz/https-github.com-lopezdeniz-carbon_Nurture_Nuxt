58614ef2870d86bebbc0209ced3d1864ca07a98c
mrLopez <lopesdenis@mail.ru>
2025-01-16 17:21:12 -0500

    Technology fix

--- pages/technology.vue
+++ pages/technology.vue
@@ -1,9 +1,50 @@
 <template>
-  <section>
-    <h1>Technology</h1>
-    <p>Discover cutting-edge sustainable technologies to help nurture your carbon footprint.</p>
-  </section>
+  <br>
+  <div class="container">
+
+
+    <!-- Первая строка: иконка и первый параграф -->
+    <div class="row align-items-center mb-4">
+      
+      <div class="col-md-2 text-center">
+        
+        <img src="/assets/images/icon-leafs.png" alt="Icon 1" class="img-fluid" />
+      </div>
+      <div class="col-md-8">
+        <h1 class="mb-5">Technology</h1>
+        <p>
+          Carbon Nurture has developed environmentally biodegradable plastics to support manufacturing of sustainable materials and regenerative economy. Our technology helps re-generating healthy soils and oceans to strengthen food chains and to re-create natural reserves. Soil-biodegradable products developed by CN help mitigating climate change by reducing carbon dioxide emissions due to sequestering carbon in soil and sediments.
+        </p>
+      </div>
+    </div>
+
+    <!-- Вторая строка: иконка и второй параграф -->
+    <div class="row align-items-center mb-4">
+      <div class="col-md-2 text-center">
+        <img src="/assets/images/icon-leafs.png" alt="Icon 2" class="img-fluid" />
+      </div>
+      <div class="col-md-8">
+        <p>
+          Environmentally biodegradable plastics are designed for specific applications with required servicelife characteristics and properties. At their end-of-life they could be recycled biologically, physically, or chemically. Biological recycling is designed to take place at natural environmental conditions and does not require high-temperature composting infrastructure.
+        </p>
+      </div>
+    </div>
+
+    <!-- Третья строка: иконка и третий параграф -->
+    <div class="row align-items-center mb-4">
+      <div class="col-md-2 text-center">
+        <img src="/assets/images/icon-leafs.png" alt="Icon 3" class="img-fluid" />
+      </div>
+      <div class="col-md-8">
+        <p>
+          The abiotic degradation is achieved by specially designed advanced redox-promoting catalyst, leading to complete conversion of polymers into biodegradable substances with high carbon utilization. Complete conversion prevents formation of persistent micro/nano-plastics.
+        </p>
+      </div>
+    </div>
+  </div>
+  <br>
 </template>
+
 
 <script setup>
 // Устанавливаем мета-данные для страницы
@@ -40,13 +81,17 @@ definePageMeta({
 
 <style scoped>
 /* Стили для страницы Technology */
-h1 {
-  font-size: 2rem;
-  color: #333;
-}
+
 p {
-  font-size: 1.25rem;
-  color: #555;
-  line-height: 1.6;
+  font-size: 21px;
 }
+
+.img-fluid {
+  max-width: 60%;
+  height: auto;
+}
+
+
 </style>
+
+
