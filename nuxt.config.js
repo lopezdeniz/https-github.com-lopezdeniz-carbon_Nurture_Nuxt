533ec3d7d00049bf6e87985711a22269c67928b6
mrLopez <lopesdenis@mail.ru>
2025-02-14 11:43:45 -0500

    add ico and config new

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,8 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-02-13", // Добавлено, чтобы убрать предупреждение
 
   css: [
-    "bootstrap/dist/css/bootstrap.min.css", 
-    "bootstrap-icons/font/bootstrap-icons.css", 
+    "bootstrap/dist/css/bootstrap.min.css",
+    "bootstrap-icons/font/bootstrap-icons.css",
     "@/assets/css/main.css"
   ],
 
@@ -12,7 +12,14 @@ export default defineNuxtConfig({
       title: "Carbon Nurture",
       meta: [
         { name: "viewport", content: "width=device-width, initial-scale=1.0" },
-        { name: "description", content: "Описание вашего сайта" }
+        { name: "description", content: "Описание вашего сайта" },
+        { name: "keywords", content: "carbon, sustainability, environment, eco-friendly, green energy" }
+      ],
+      link: [
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+        { rel: "manifest", href: "/site.webmanifest" }
       ],
       script: [
         {
